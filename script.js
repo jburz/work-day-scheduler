b70edc0258143c8b0e2a4c1b685fe0db9ffc2016
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-08-17 22:22:36 -0600

    Added hour blocks for 8 hours, input blocks for text and save button to store inputs in local storage

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+let hoursCounter = 9;
+let hourBlock;
+let time;
+let event;
+let input;
+
 let now = moment();
-console.log(now.format());
-$('#currentDay').text(now.format('dddd') + ", " + now.format('LL'));
+$('#currentDay').text(now.format('dddd, MMMM Do'));
+
+for (let i = 1; i < 9; i++) {
+    hourBlock = $('#hourBlock' + i);
+    time = moment().hour(hoursCounter).minute(0).second(0).format('LT');
+    hourBlock.text(time);
+    hoursCounter++;
+    $('#event' + i).val(localStorage.getItem('event' + i));
+    $('#event' + i).attr('class', 'future');
+}
+
+$('#saveBtn1').on('click', function (event) {
+    input = $('#event1').val();
+    console.log(input);
+    localStorage.setItem('event1', input);
+})
+$('#saveBtn2').on('click', function (event) {
+    input = $('#event2').val();
+    console.log(input);
+    localStorage.setItem('event2', input);
+})
+$('#saveBtn3').on('click', function (event) {
+    input = $('#event3').val();
+    console.log(input);
+    localStorage.setItem('event3', input);
+})
+$('#saveBtn4').on('click', function (event) {
+    input = $('#event4').val();
+    console.log(input);
+    localStorage.setItem('event4', input);
+})
+$('#saveBtn5').on('click', function (event) {
+    input = $('#event5').val();
+    console.log(input);
+    localStorage.setItem('event5', input);
+})
+$('#saveBtn6').on('click', function (event) {
+    input = $('#event6').val();
+    console.log(input);
+    localStorage.setItem('event6', input);
+})
+$('#saveBtn7').on('click', function (event) {
+    input = $('#event7').val();
+    console.log(input);
+    localStorage.setItem('event7', input);
+})
+$('#saveBtn8').on('click', function (event) {
+    input = $('#event8').val();
+    console.log(input);
+    localStorage.setItem('event8', input);
+})
+
+
+
