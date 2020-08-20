6f08b33205fe4f5d8e0e086b0af49a7bb64d4ef3
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-08-19 18:23:00 -0600

    added save icons and improved time formatting to display colors in event blocks

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-let hoursCounter = 9;
-let hourBlock;
-let time;
-let event;
+let hoursCounter = 14;
+let hourBlockElem;
+let timeBlockDisplay;
+let timeBlockCompare;
+let timeBlockPlusCompare;
+let eventLog;
 let input;
 
 let now = moment();
 $('#currentDay').text(now.format('dddd, MMMM Do'));
 
 for (let i = 1; i < 9; i++) {
-    hourBlock = $('#hourBlock' + i);
-    time = moment().hour(hoursCounter).minute(0).second(0).format('LT');
-    hourBlock.text(time);
+    hourBlockElem = $('#hourBlock' + i);
+    timeBlockDisplay = moment().hour(hoursCounter).format('hA');
+    timeBlockCompare = moment().hour(hoursCounter).format('H');
+    timeBlockPlusCompare = moment().hour(hoursCounter + 1).format('H');
+    hourBlockElem.text(timeBlockDisplay);
+    $('#event' + i).val(localStorage.getItem('eventLog' + i));
+
+    console.log('Current time: ' + now.hour());
+    console.log('Time Block: ' + timeBlockCompare);
+
+    if (now.hour() < timeBlockCompare) {
+        $('#event' + i).attr('class', 'future');
+        console.log('Future');
+    } else if (now.hour() >= timeBlockCompare && now.hour() < timeBlockPlusCompare) {
+        $('#event' + i).attr('class', 'present');
+        console.log('Present');
+    } else if (now.hour() > timeBlockPlusCompare) {
+        $('#event' + i).attr('class', 'past');
+        console.log('Past');
+    }
+
     hoursCounter++;
-    $('#event' + i).val(localStorage.getItem('event' + i));
-    $('#event' + i).attr('class', 'future');
-    $('#saveBtn'+i).on('click', function (event) {
-        input = $('#event'+i).val();
+
+
+    $('#saveBtn' + i).on('click', function (event) {
+        input = $('#event' + i).val();
         console.log(input);
-        localStorage.setItem('event'+i, input);
+        localStorage.setItem('eventLog' + i, input);
     });
 }
-
 
 
 
