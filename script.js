6fc3cedcf223863a29961eb6b0a979c3d835e916
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-08-19 18:32:11 -0600

    remove consolelogs and clean up code format and spacing

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let hoursCounter = 14;
+let hoursCounter = 9;
 let hourBlockElem;
 let timeBlockDisplay;
 let timeBlockCompare;
 let timeBlockPlusCompare;
 let eventLog;
 let input;
-
 let now = moment();
+
 $('#currentDay').text(now.format('dddd, MMMM Do'));
 
 for (let i = 1; i < 9; i++) {
@@ -17,26 +17,18 @@ for (let i = 1; i < 9; i++) {
     hourBlockElem.text(timeBlockDisplay);
     $('#event' + i).val(localStorage.getItem('eventLog' + i));
 
-    console.log('Current time: ' + now.hour());
-    console.log('Time Block: ' + timeBlockCompare);
-
     if (now.hour() < timeBlockCompare) {
         $('#event' + i).attr('class', 'future');
-        console.log('Future');
     } else if (now.hour() >= timeBlockCompare && now.hour() < timeBlockPlusCompare) {
         $('#event' + i).attr('class', 'present');
-        console.log('Present');
     } else if (now.hour() > timeBlockCompare) {
         $('#event' + i).attr('class', 'past');
-        console.log('Past');
     }
 
     hoursCounter++;
 
-
     $('#saveBtn' + i).on('click', function (event) {
         input = $('#event' + i).val();
-        console.log(input);
         localStorage.setItem('eventLog' + i, input);
     });
 }
