63f6c9cc3b7aca70b3163c62dda49252a5a69c38
Jake Burzlaff <jake.bilbao.04.06@gmail.com>
2020-08-22 22:46:43 -0600

    added notes to javascript, created readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# work-day-scheduler
+# Day Scheduler
+
+This is a work simple work scheduler that I created to allow the user to plan out their day and schedule events during the workday.  This scheduler assumes a work day from 9 to 5.  
+
+## Features
+This application has a number of features, including:
+
+1. Current date is displayed in the header.
+2. Each row has a time display, input box for reminders and a save button.
+3.  The input boxes will be highlight depending on what the current time is.
+    - The box will be gray if it is in the past
+    - It will be red if the current time falls within that block of time
+    - Future time blocks will be highlighted green.
+    - The highlighting will update every time the page is refreshed.\
+4. There is a save button to save the event in memory.  This will be stored even if the page is refreshed.
+ 
+
+Click to navigate to the [work day scheduler](https://jburz.github.io/work-day-scheduler)
+
+
+## Screenshot
+![work day scheduler](./assets/work-day-scheduler.png)
+
+## Tools
+
+The layout and design for this app uses the css framework [bootstrap](https://getbootstrap.com/docs/4.5/getting-started/introduction/) as well as some custom styling.
+
+All of the date and time formatting and display was done with [moment.js](https://momentjs.com/).
+
+I really tried to focus on eliminating repetition and keep the javascript code as dry as possible within this app.
+
+
+## Credits
+
+This app was created entirely by [Jake Burzlaff](https://www.github.com/jburz).
--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-let hoursCounter = 16;
+let hoursCounter = 9;
 let hourBlockElem;
 let timeBlockDisplay;
-let timeBlockCompare;
-let timeBlockPlusCompare;
+let timeBlockStartCompare;
+let timeBlockEndCompare;
 let eventLog;
 let input;
 let now = moment();
+let interval;
 
 $('#currentDay').text(now.format('dddd, MMMM Do'));
 
-for (let i = 1; i < 9; i++) {
+//iterate through each timeblock element
+for (let i = 1; i < 17; i++) {
+    //grab the element
     hourBlockElem = $('#hourBlock' + i);
+
+    //format moment to display hour and am/pm
     timeBlockDisplay = moment().hour(hoursCounter).format('hA');
-    timeBlockCompare = moment().hour(hoursCounter).format('H');
-    timeBlockPlusCompare = moment().hour(hoursCounter + 1).format('H');
+
+    //format moment in military time for start of block
+    timeBlockStartCompare = moment().hour(hoursCounter).format('H');
+
+    //format moment in military time for end of block
+    timeBlockEndCompare = moment().hour(hoursCounter + 1).format('H');
+
     hourBlockElem.text(timeBlockDisplay);
+    
+    //Display any value in local storage
     $('#event' + i).val(localStorage.getItem('eventLog' + i));
 
-    if (now.hour() < timeBlockCompare) {
-        $('#event' + i).attr('class', 'future col-sm-10');
-    } else if (now.hour() >= timeBlockCompare && now.hour() < timeBlockPlusCompare) {
-        $('#event' + i).attr('class', 'present col-sm-10');
-    } else if (now.hour() > timeBlockCompare) {
-        $('#event' + i).attr('class', 'past col-sm-10');
+    //checks current time against timeblock start and end to set background color 
+    if (now.hour() < timeBlockStartCompare) {
+        $('#event' + i).addClass('future');
+    } else if (now.hour() >= timeBlockStartCompare && now.hour() < timeBlockEndCompare) {
+        $('#event' + i).addClass('present');
+    } else if (now.hour() > timeBlockStartCompare) {
+        $('#event' + i).addClass('past');
     }
 
     hoursCounter++;
